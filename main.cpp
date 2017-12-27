7d42187c8512e46f90d21f2d43293dd4833b1ca6
evgenykol <edkolotilov@gmail.com>
2017-12-27 23:02:14 +0300

    Похоже что вот все-таки теперь сделал аллокатор. Проверил на мапе. Выделяется и удаляется одинаковое количество элементов. Все адреса, котоыре были выделены - в последствии удалились

--- main.cpp
+++ main.cpp
@@ -4,10 +4,51 @@
 
 #include "version.h"
 
+#define ENABLE_LOGGING
+#define SIZE 10
+
+
 using namespace std;
 
 template <typename T>
 struct logging_allocator
+{
+    using value_type = T;
+
+    T* allocate(size_t n)
+    {
+        auto p = malloc(n * sizeof(T));
+        if(!p)
+        {
+            throw bad_alloc();
+        }
+        cout << "allocate " << n << "\t\t at " << p << " sizeof " << sizeof(T) << endl;
+        return reinterpret_cast<T *>(p);
+    }
+
+    void deallocate(T* p, size_t n)
+    {
+        cout << "deallocate " << n << "\t at " << p << endl;
+        free(p);
+    }
+
+    template<typename U, typename ...Args>
+    void construct(U* p, Args&& ...args)
+    {
+        cout << "construct " << "\t\t at " << p << " sizeof " << sizeof(U) << endl;
+        new(p) U(forward<Args>(args) ...);
+    }
+
+    void destroy(T* p)
+    {
+        p->~T();
+        cout << "destroy " << "\t\t at " << p << endl;
+    }
+
+};
+
+template <typename T>
+struct custom_allocator
 {
     using value_type = T;
     int block_size;
@@ -16,13 +57,16 @@ struct logging_allocator
     T *head_ptr;
     void *p;
 
-    logging_allocator(int block_size_ = 10)
+    custom_allocator(int block_size_ = SIZE)
     {
         block_size = block_size_;
         allocated_blocks = 0;
         head = 0;
+        head_ptr = nullptr;
         p = nullptr;
-        cout << "logging_allocator(int) ctor, block_size = " << block_size << endl;
+#ifdef ENABLE_LOGGING
+        cout << "custom_allocator ctor, block_size = " << block_size << endl;
+#endif
     }
 
     T* allocate(size_t n)
@@ -30,20 +74,30 @@ struct logging_allocator
         if (allocated_blocks == 0)
         {
             p = malloc(block_size * sizeof(T));
-            cout << "allocate malloc " << block_size << "\t at " << p << endl;
             if(!p)
             {
                 throw bad_alloc();
             }
             allocated_blocks++;
-            head++;
+
             head_ptr = reinterpret_cast<T *>(p);
+#ifdef ENABLE_LOGGING
+            cout << "allocate malloc " << block_size << "\t at " << p << " head_ptr " << head_ptr << endl;
+#endif
+        }
+        else if (head >= block_size)
+        {
+#ifdef ENABLE_LOGGING
+            cout << "allocate ptr reached top" << "\t at " << head_ptr << endl;
+#endif
         }
         else
         {
             head += n;
             head_ptr += n * sizeof(T);
-            cout << "allocate move ptr " << n << "\t at " << head_ptr << endl;
+#ifdef ENABLE_LOGGING
+            cout << "allocate move head " << n << "\t at " << head_ptr << endl;
+#endif
         }
 
         return head_ptr;
@@ -51,31 +105,40 @@ struct logging_allocator
 
     void deallocate(T* pp, size_t n)
     {
+        if(allocated_blocks && (head <= 0))
+        {
+#ifdef ENABLE_LOGGING
+            cout << "deallocate free " << n << "\t at " << pp << endl;
+#endif
+            free(p);
+            allocated_blocks--;
+        }
+
         if (head > 0)
         {
             head -= n;
             head_ptr -= n * sizeof(T);
-            cout << "deallocate move ptr " << n << "\t at " << head_ptr << endl;
-        }
-
-        if(head <= 0)
-        {
-            cout << "deallocate free " << n << "\t at " << p << endl;
-            free(p);
+#ifdef ENABLE_LOGGING
+            cout << "deallocate move head " << n << "\t at " << pp << endl;
+#endif
         }
     }
 
     template<typename U, typename ...Args>
     void construct(U* p, Args&& ...args)
     {
+#ifdef ENABLE_LOGGING
         cout << "construct " << "\t\t at " << p << endl;
+#endif
         new(p) U(forward<Args>(args) ...);
     }
 
     void destroy(T* p)
     {
         p->~T();
+#ifdef ENABLE_LOGGING
         cout << "destroy " << "\t\t at " << p << endl;
+#endif
     }
 
 };
@@ -92,9 +155,10 @@ int main()
 {
     cout << "version " << version() << endl << endl;
 
-//    auto v = vector<int, logging_allocator<int>>{};
+//    auto v = vector<int, custom_allocator<int>>{};
+//    v.reserve(10);
 
-//    for(size_t i = 0; i < 5; ++i)
+//    for(size_t i = 0; i < 10; ++i)
 //    {
 //        v.emplace_back(i);
 //    }
@@ -106,26 +170,26 @@ int main()
 
     cout << endl << "map1:" << endl;
     auto m1 = map<int, int>{};
-    for(size_t i = 0; i < 10; ++i)
+    for(size_t i = 0; i < SIZE; ++i)
     {
-        m1.insert(pair<int, int>{i, fact(i)});
+        m1.emplace(pair<int, int>{i, fact(i)});
     }
 
     for(auto mm : m1)
     {
-        cout << mm.first << "\t" << mm.second << endl;
+        cout << mm.first << " " << mm.second << endl;
     }
 
     cout << endl << "map2:" << endl;
-    auto m2 = map<int, int, less<int>, logging_allocator<pair<const int, int>>>{};
-    for(size_t i = 0; i < 10; ++i)
+    auto m2 = map<int, int, less<int>, custom_allocator<pair<const int, int>>>{};
+    for(size_t i = 0; i < SIZE; ++i)
     {
-        m2.insert(pair<int, int>{i, fact(i)});
+        m2.emplace(pair<int, int>{i, fact(i)});
     }
 
     for(auto mm : m2)
     {
-        cout << mm.first << "\t" << mm.second << endl;
+        cout << mm.first << " " << mm.second << endl;
     }
 
     return 0;
